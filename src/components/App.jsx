35045e81e4c27dbe70311eedd97350dce5093628
SidC625 <sid.blueurbn@gmail.com>
2023-03-25 20:40:17 +0530

    gray section addition

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./Navbar";
 import LP_PG_0 from "./Page0_info";
+import TechCrunch from "./TechCrunch";
 
 function App(){
     return <div>
                 <div class="blue_grad_sec_pos">
                     <Navbar />
                     <LP_PG_0 />
+                </div>
+                <div class="gray_div">
+                    <TechCrunch />
                 </div>
             </div>
 };
---
+++ src/components/TechCrunch.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TechCrunch(){
+    return <div>
+        <img src="./logo/"></img>
+    </div>
+};
+
+export default TechCrunch;
