26aad854a82aff56689f71be94fb9507c6aee413
SidC625 <sid.blueurbn@gmail.com>
2023-03-29 21:27:43 +0530

    Footer addition

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,10 +24,20 @@ function App(){
                     <h1 className="dont_take_our_word">Don’t take our word for it</h1>
                     <h1 className="see_for_yourself">See For Yourself</h1>
                     <img className="blog_1_pos" src="./logo/Blog_img_1.png"></img>
+                        <span className="blog_1_pos blog_text_h1" style={{top : "430px"}}>Smart Building Energy Management systems can lower energy consumption by 30%</span>
+                        <span className="blog_1_pos blog_text_h2" style={{top : "660px"}}>Learn more about how IoT building systems can have a payback period of 10 months</span>
                     <img className="blog_2_pos" src="./logo/Blog_img_2.png"></img>
+                        <span className="blog_2_pos blog_text_h1" style={{top : "310px"}}>Geotagging with an address</span>
+                        <span className="blog_2_pos blog_text_h2" style={{top : "420px"}}>Maximize your solar investment</span>
                     <img className="blog_3_pos" src="./logo/Blog_img_3.png"></img>
+                        <span className="blog_3_pos blog_text_h1" style={{top : "310px"}}>High performance buildings</span>
+                        <span className="blog_3_pos blog_text_h2" style={{top : "420px"}}>No Capital Costs</span>
                     <img className="blog_4_pos" src="./logo/Blog_img_4.png"></img>
+                        <span className="blog_4_pos blog_text_h1" style={{top : "710px"}}>The story lies in the Data</span>
+                        <span className="blog_4_pos blog_text_h2" style={{top : "950px"}}>Whether it’s increase in NOI or reducing emissions, data is paramount   </span>
                     <img className="blog_5_pos" src="./logo/Blog_img_5.png"></img>
+                        <span className="blog_5_pos blog_text_h1" style={{top : "830px"}}>Profitable ESG, done right</span>
+                        <span className="blog_5_pos blog_text_h2" style={{top : "960px"}}>Learn about how we enable enterprises to measure, report and take action with ease  </span>
                 </div>
 
                 <div>
@@ -58,7 +68,11 @@ function App(){
                             </form>
                         </span>
                     </div>
-                    <span className="form_next"></span>
+                    <span className="form_next" style={{zIndex : '-1'}}></span>
+                    <div className="footer_blue_ribbon"></div>
+                    <footer className="footer_bkg">
+
+                    </footer>
 
                 </div>
 
